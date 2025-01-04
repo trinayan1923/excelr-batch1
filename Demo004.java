88999d1b3b097f183a4a674bb4729069d773904a
trinayan1923 <151130655+trinayan1923@users.noreply.github.com>
2025-01-04 12:18:51 +0530

    Add files via upload

---
+++ Demo004.java
@@ -0,0 +1,9 @@
+package core_java;
+
+public class Demo004 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Demo005.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class Demo005 {
+    public static void main(String[] args) {
+        String username = "user";
+        String password = "pass";
+        while ((username == "user") && (password == "pass")) {
+            System.out.println("hello user happy new year");
+            break;
+            
+        }
+    }
+}
