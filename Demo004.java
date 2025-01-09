c811807b300e96e3c571deb6c979469b499d0a21
trinayan1923 <151130655+trinayan1923@users.noreply.github.com>
2025-01-09 10:28:35 +0530

    Delete Demo004.java

--- Demo004.java
+++
@@ -1,9 +0,0 @@
-package core_java;
-
-public class Demo004 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++){
-            System.out.println(i);
-        }
-    }
-}
