f182d8ee5970d0d922404c70c9d680fbd38bd2bb
trinayan1923 <151130655+trinayan1923@users.noreply.github.com>
2025-01-21 09:32:27 +0530

    reverse of input

---
+++ core_java/Demo0025.java
@@ -0,0 +1,14 @@
+package core_java;
+
+import java.util.Scanner;
+
+public class Demo0025 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Name :");
+        String input = sc.nextLine();
+        String reverse = new StringBuilder(input).reverse().toString();
+        System.out.println("Reverse of the input is:"+reverse);
+        
+    } 
+}
