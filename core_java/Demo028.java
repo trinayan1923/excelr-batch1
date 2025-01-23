0ec206f27460559a1265a4cea069cb6a603b1757
trinayan1923 <151130655+trinayan1923@users.noreply.github.com>
2025-01-23 10:21:46 +0530

    fibonacci

--- core_java/Demo028.java
+++ core_java/Demo028.java
@@ -3,15 +3,12 @@ package core_java;
 import java.util.Scanner;
 
 public class Demo028 {
-
-    
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of terms in the Fibonacci series: ");
