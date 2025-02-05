3f4688605e4d610f21b5931db40fe542aa8cce0f
trinayan1923 <151130655+trinayan1923@users.noreply.github.com>
2025-02-05 10:52:14 +0530

    leap year

---
+++ core_java/demo046.java
@@ -0,0 +1,19 @@
+package core_java;
+
+import java.util.Scanner;
+public class demo046 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        // Check if the year is a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
+
+
